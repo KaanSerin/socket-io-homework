39e37196177a9dd1b511ef37af99cfbdb5acc332
Kaan Serin <kaanserin113@gmail.com>
2021-02-18 16:49:37 +0200

    Implement add nickname feature

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,14 +1,34 @@
+let nickName = "";
+
+const nickInput = document.getElementById("nickname");
+const nickContainer = document.getElementById("nickname-container");
 const messages = document.querySelector("#messages");
+const userNickname = document.getElementById("user-nickname");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
+input.disabled = true;
+
+nickContainer.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (nickInput.value.length < 2) return;
+  nickName = nickInput.value;
+  userNickname.textContent = nickInput.value + ":";
+  input.disabled = false;
+
+  nickContainer.hidden = true;
+});
+
 const socket = io();
+
+// Contruct message with users name
+const createMessage = (msg) => `${nickName}: ${msg}`;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(input.value);
   if (input.value) {
-    socket.emit("chat message", input.value);
+    socket.emit("chat message", createMessage(input.value));
     input.value = "";
   }
 });
